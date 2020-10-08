74abe08a632b9fa3d78cf0eec581b7a80763777f
Danny Libin <dlibinrx@gmail.com>
2020-10-08 17:31:24 -0500

    contact form in footer complete with validation

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import {
   AppBar,
   Button,
+  CircularProgress,
   createMuiTheme,
   Drawer,
   makeStyles,
@@ -100,6 +101,7 @@ const Layout = ({ path, children }) => {
 
   const [snackOpen, setSnackOpen] = React.useState(false);
   const [snackMsg, setSnackMsg] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
 
   const nameFirstRef = useRef(null);
   const nameLastRef = useRef(null);
@@ -107,23 +109,52 @@ const Layout = ({ path, children }) => {
   const commentRef = useRef(null);
 
   const handleEmailSubmit = () => {
+    // Prevent repetitive submissions
+    if (loading) return;
+
     // Hacky access of material-ui input values for speed
     const mailData = {
       first: nameFirstRef.current.children[1].firstElementChild.value,
       last: nameLastRef.current.children[1].firstElementChild.value,
       email: emailRef.current.children[1].firstElementChild.value,
-      body: commentRef.current.children[1].firstElementChild.value,
-      subject: ''
+      body: commentRef.current.children[1].firstElementChild.value
+      //subject: ''
     };
-    console.log(mailData);
+
+    let emailReg = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
+
+    if (
+      mailData.first === '' ||
+      mailData.last === '' ||
+      mailData.email === '' ||
+      mailData.body === ''
+    ) {
+      setSnackMsg('Please fill out all fields');
+      setSnackOpen(true);
+      return;
+    } else if (!emailReg.exec(mailData.email)) {
+      setSnackMsg('Please enter a valid email address');
+      setSnackOpen(true);
+      return;
+    }
+
+    setLoading(true);
+
     sendMail(mailData)
       .then((res) => {
         setSnackMsg('Email sent, thanks for contacting us!');
         setSnackOpen(true);
+        nameFirstRef.current.children[1].firstElementChild.value = '';
+        nameLastRef.current.children[1].firstElementChild.value = '';
+        emailRef.current.children[1].firstElementChild.value = '';
+        commentRef.current.children[1].firstElementChild.value = '';
+        setLoading(false);
       })
       .catch((e) => {
+        console.log(e);
         setSnackMsg('Email error, please try again later.');
         setSnackOpen(true);
+        setLoading(false);
       });
   };
 
@@ -364,8 +395,22 @@ const Layout = ({ path, children }) => {
                 color="primary"
                 id="contact-submit"
                 onClick={handleEmailSubmit}
+                style={{ height: '36px' }}
               >
-                Submit
+                {loading ? (
+                  <div
+                    style={{
+                      marginRight: '5px',
+                      position: 'relative',
+                      top: '2px'
+                    }}
+                  >
+                    <CircularProgress
+                      style={{ width: '20px', height: '20px' }}
+                    />
+                  </div>
+                ) : null}
+                <div>Submit</div>
               </Button>
             </form>
           </div>
--- src/util/contact.js
+++ src/util/contact.js
@@ -1,6 +1,7 @@
 export function sendMail(mailData) {
-  return Promise.resolve();
-  // return fetch('https://us-central1-dlibin-api-ca89a.cloudfunctions.net', {
+  return new Promise((resolve) => setTimeout(resolve, 2000));
+  // return fetch('http://localhost:5001/dlibin-api-ca89a/us-central1/postEmail', { // testing only
+  //return fetch('https://us-central1-dlibin-api-ca89a.cloudfunctions.net/postEmail', {
   //   method: 'POST',
   //   body: JSON.stringify(mailData)
   // });
